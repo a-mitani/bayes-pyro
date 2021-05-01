7487e4ab91198672e5b00fefcfb0112c4321c6d6
a-mitani <a-mitani@users.noreply.github.com>
2021-05-02 08:50:51 +0900

    docsディレクトリ削除

--- docs/ru.search-data.min.5b39e0a8766e19ce363bb0bd2645e46df139a7a5dfe28707f3579f617ab7b59a.js
+++
@@ -1,1 +0,0 @@
-'use strict';(function(){const indexCfg={split:/[^a-zа-яё0-9\w]/gi};indexCfg.doc={id:'id',field:['title','content'],store:['title','href','section'],};const index=FlexSearch.create('balance',indexCfg);window.bookSearchIndex=index;})();
--- docs/zh.search-data.min.b26ef3cfbf84ce348d931601b11a719ad77f97abbd92a1a1179a0be5b71b8d45.js
+++
@@ -1,1 +0,0 @@
-'use strict';(function(){const indexCfg={encode:false,tokenize:function(str){return str.replace(/[\x00-\x7F]/g,'').split('');}};indexCfg.doc={id:'id',field:['title','content'],store:['title','href','section'],};const index=FlexSearch.create('balance',indexCfg);window.bookSearchIndex=index;})();
